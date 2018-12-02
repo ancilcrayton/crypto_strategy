3f6c6c5418fecfd301d0a16ebbd24798dbc9f69f
ancilcrayton <ancil.crayton@ucdconnect.ie>
2018-12-02 02:10:54 +0000

    Add function to implement Bollinger Strategy

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def compute_bollinger(df, window_size, no_std_devs):
-    """Short summary.
+    """This function computes the Bollinger bands.
 
     Parameters
     ----------
@@ -25,3 +25,45 @@ def compute_bollinger(df, window_size, no_std_devs):
     bollinger_low = df_mean - (df_std*no_std_devs)
     bollinger_high = df_mean + (df_std*no_std_devs)
     return bollinger_low, bollinger_high
+
+def bollinger_strategy(df, window_size, no_std_devs):
+    """This function performs the Bollinger Strategy and returns a dataframe that includes the relevant positions.
+
+    Parameters
+    ----------
+    df : DataFrame
+        Dataframe which includes stock price.
+
+    Returns
+    -------
+    DataFrame
+        Returns a DataFrame which includes the position based on the Bollinger Strategy.
+
+    """
+    # Compute Bollinger bands using above function
+    df['Bollinger Low'], df['Bollinger High'] = compute_bollinger(df=df, window_size=window_size, no_std_devs=no_std_devs)
+
+    # Initialize an empty Position column to fill in
+    df['Position'] = None
+
+    # Set initial status to open and iterate over each row index
+    mode = 'open'
+    for index in range(len(df)):
+        if index == 0:
+            continue
+
+        # Grab current and previous row
+        row = df.iloc[index]
+        prev_row = df.iloc[index-1]
+
+        # Hold long position
+        if mode == 'open' and row['Open'] < row['Bollinger Low'] and prev_row['Open'] > prev_row['Bollinger Low']:
+            df.iloc[index, df.columns.get_loc('Position')] = 1
+            mode = 'close'
+
+        # Hold a short position
+        if mode == 'close' and row['Open'] > row['Bollinger High'] and prev_row['Open'] < prev_row['Bollinger High']:
+            df.iloc[index, df.columns.get_loc('Position')] = -1
+            mode = 'open'
+
+    return df
